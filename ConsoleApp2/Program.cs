f0999547e3a5d8dd82dbbe98777d59ba8bbd30a5
zoro <zoro@DESKTOP-9QIHMFK>
2022-01-13 15:15:01 +0200

    Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float baseNum;
+            Console.Write("Enter the number: ");
+           bool isConverted= float.TryParse(Console.ReadLine(),out baseNum);
+            if (isConverted)
+            {
+                for (int num = 1; num <= 12; num++)
+                {
+                    string str;
+                    str = baseNum + "x" + num + " = ";
+                    float r = baseNum * num;
+                    Console.WriteLine(str + r);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter valid number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
